96034fbfb2e32e271a75282848b50a78d041fe53
leocyx <leo8400725@yahoo.com.tw>
2017-09-24 16:09:57 +0800

    gitignore test

--- test/manager.js
+++ test/manager.js
@@ -1,18 +1,21 @@
 var M = module.exports = {}
 
-var leo={id:'leo',password:'123456'}
-var snoopy={id:'snoppy',passowrd:'123456'}
-var usermap={
-    'leo':leo,
-    'snoopy':snoopy,
-    }
-M.login = function(id, password){
-    var user=usermap[id]
-    if(user.password===password){
-        return user
-    } 
+var ccc = {id: 'ccc', password: '1234567'}
+var snoopy = {id: 'snoopy', password: '7654321'}
+
+var userMap = {
+  'ccc': ccc,
+  'snoopy': snoopy
 }
-var user1 = M.login('leo','1234')
-console.log("user1=%j",user1)
-var user2 = M.login('leo','123456')
-console.log("user2=%j",user2)
+
+M.login = function (id, password) {
+  var user = userMap[id]
+  if (typeof user !== 'undefined' && user.password === password) {
+    return user
+  }
+}
+
+// var user1 = M.login('ccc', '1111')
+// console.log('user1=%j', user1)
+// var user2 = M.login('ccc', '1234567')
+// console.log('user2=%j', user2)
